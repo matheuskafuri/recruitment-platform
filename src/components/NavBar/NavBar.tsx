a02e1259e7c4adc7bec302a6c49a2e9774c7b817
Matheus Ramos Kafuri <m.kafuri@g4educacao.com>
2022-08-07 12:25:14 -0300

    .

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -126,7 +126,7 @@ const NavBar = () => {
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
