f96dcf09b34a4bed1b45c24e5cc7c2a8aa4cf773
Matheus Ramos Kafuri <m.kafuri@g4educacao.com>
2022-08-07 11:57:26 -0300

    .

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -90,7 +90,7 @@ const NavBar = () => {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color="primary"
             >
               <MenuIcon />
             </IconButton>
